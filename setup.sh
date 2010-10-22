46e56c88d3c200767ba555089436f8c29fac5e8a
Richard Crowley <r@rcrowley.org>
2010-10-22 12:58:25 -0700

    Slight refactor.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
-set -e
+# Setup DevStructure.
 
-# Do you have an account?
-# Collect relevant information.
-# POST it up.
-# Explain any errors and exit.
-# Populate ~/.token.
-# Display a welcome message.
-
+# Gather credentials.
 TMPNAME=$(mktemp devstructure-XXXXXXXXXX)
 trap "rm -rf \"$TMPNAME\"" 0
-echo "\033[1mWELCOME TO DEVSTRUCTURE\033[0m"
+echo
+echo "\033[1mGET STARTED WITH DEVSTRUCTURE\033[0m"
+echo
 while true
 do
-	read -p "Do you already have an account? [yN] " YESNO
+	read -p "Do you have an account? (Ctrl+C to postpone.) [yN] " YESNO
 	[ -z "$YESNO" ] && YESNO="n"
 	case "$YESNO" in
 
+		# Sign in.
 		y|Y)
 			read -p "Email or login: " VALUE
 			stty -echo
@@ -26,12 +23,12 @@ do
 				*@*) KEY="email";;
 				*) KEY="login";;
 			esac
-			CODE=$(curl -s -w '%{http_code}\n' -o "$TMPNAME" \
+			CODE="$(curl -s -w '%{http_code}\n' -o "$TMPNAME" \
 				-d "$KEY"="$VALUE" \
 				-d password="$PASSWORD" \
-				"https://api.devstructure.com/sign_in")
-			;;
+				"https://api.devstructure.com/sign_in" || echo -)";;
 
+		# Sign up.
 		n|N)
 			read -p "Email: " EMAIL
 			read -p "Login: " LOGIN
@@ -45,33 +42,68 @@ do
 				echo "Please confirm your password." >&2
 				continue
 			}
-			CODE=$(curl -s -w '%{http_code}\n' -o "$TMPNAME" \
+			CODE="$(curl -s -w '%{http_code}\n' -o "$TMPNAME" \
 				-d email="$EMAIL" \
 				-d login="$LOGIN" \
 				-d password="$PASSWORD" \
-				"https://api.devstructure.com/sign_up")
-			;;
+				"https://api.devstructure.com/sign_up" || echo -)";;
 
+		# Ask again.
 		*) continue;;
 
 	esac
 
+	# Successful sign in or sign up breaks out of the loop.  Everyone
+	# else gets a message and heads back to the top.
 	case "$CODE" in
-		200) break;;
-		400) echo "Invalid account credentials, please try again." >&2;;
-		401) echo "Password doesn't match, please try again.">&2;;
-		409) echo "That email or login is already registered." >&2;;
-		*) echo "DevStructure is having problems." >&2;;
+		200) ;;
+		400)
+			echo "Invalid account credentials, please try again." >&2
+			continue;;
+		401)
+			echo "Password doesn't match, please try again.">&2
+			continue;;
+		409)
+			echo "That email or login is already registered." >&2
+			continue;;
+		-)
+			echo "Please try again when you're connected to the Internet." >&2
+			echo "  sh /etc/profile.d/setup.sh" >&2
+			break;;
+		*)
+			echo "DevStructure is having problems." >&2
+			continue;;
 	esac
 
-done
-TOKEN=$(tr , \\n <"$TMPNAME" | tr -d \" | grep ^token | cut -c7-)
-echo "TOKEN: $TOKEN"
-#sudo touch /etc/token
-#echo "$TOKEN" >~/.token
-echo "You're good to go!"
-# TODO More getting started material.
+	# Parse out the DevStructure API token.
+	TOKEN=$(tr , \\n <"$TMPNAME" | tr -d \" | grep ^token | cut -c7-)
+	[ -z "$TOKEN" ] && {
+		echo "No DevStructure API token found." >&2
+		echo "Please contact us at \033[4msupport@devstructure.com\033[0m." >&2
+		break
+	}
 
-# Since this run has clearly been successful, there's no reason for this
-# file to exist anymore.
-#sudo rm $0
+	# Store the DevStructure API token for later.
+	sudo touch /etc/token
+	echo "$TOKEN" >~/.token
+
+	# Now the complete welcome message.
+	echo
+	echo "\033[1mWELCOME TO DEVSTRUCTURE\033[0m"
+	echo
+	echo "Get started by creating a sandbox: " \
+		"\033[4msandbox create my-first-sandbox\033[0m"
+	echo
+	echo "Work there to keep your server clean: " \
+		"\033[4msandbox use my-first-sandbox\033[0m"
+	echo
+	echo "And when you're ready to deploy: " \
+		"\033[4mblueprint create my-first-blueprint\033[0m"
+	echo
+	echo "Learn more at \033[4mhttp://docs.devstructure.com/start\033[0m"
+
+	# This run was successful so setup.sh is no longer necessary.
+	sudo rm /etc/profile.d/setup.sh
+
+	break
+done
